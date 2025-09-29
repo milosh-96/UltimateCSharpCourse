cc6773b56dab7a495d2e5a6d1c54fe42b043e74d
Miloš Jovanović <milos.jovanovic@lombiq.com>
2025-09-29 16:43:01 +0200

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+
+            Console.WriteLine("Input the first number: ");
+            string firstNumberInput = Console.ReadLine();
+            int firstNumber = int.Parse(firstNumberInput);
+
+            Console.WriteLine("Input the second number: ");
+            string secondNumberInput = Console.ReadLine();
+            int secondNumber = int.Parse(secondNumberInput);
+
+            Console.WriteLine("What do you want to do with those numbers?");
+            Console.WriteLine("[A]dd");
+            Console.WriteLine("[S]ubtract");
+            Console.WriteLine("[M]ultiply");
+
+            bool isCalculated = false;
+            string @operator = "";
+            int result = 0;
+
+
+            string userChoice = Console.ReadLine();
+
+            if(userChoice.ToLower() == "a")
+            {
+                @operator = "+";
+                result = Add(firstNumber, secondNumber);
+                isCalculated = true;
+            }
+            else if(userChoice.ToLower() == "s")
+            {
+                @operator = "-";
+                result = Subtract(firstNumber, secondNumber);
+                isCalculated = true;
+            }
+            else if(userChoice.ToLower() == "m")
+            {
+                @operator = "*";
+                result = Multiply(firstNumber, secondNumber);
+                isCalculated = true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid option.");
+            }
+
+            if(isCalculated)
+            {
+                Console.WriteLine(PrintResult(firstNumber, secondNumber, @operator, result));
+            }
+
+            Console.WriteLine("Press any key to close.");
+            Console.ReadKey();
+        }
+        
+       
+        public static string PrintResult(int firstNumber, int secondNumber, string @operator, int result)
+        {
+            return firstNumber + " " + @operator + " " + secondNumber + " = " + result;
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ ToDoList/Program.cs
@@ -0,0 +1,117 @@
+﻿namespace ToDoList;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        bool isRunning = true;
+        List<string> todoItems = new List<string>();
+
+        do
+        {
+            DisplayMenu();
+            string userChoiceInput = Console.ReadLine();
+            char.TryParse(userChoiceInput.ToLower(), out char userChoice);
+
+
+            Console.WriteLine(); // just some spacing
+            switch (userChoice)
+            {
+                case 's':
+                    ListTodoItems(todoItems);
+                    break;
+                case 'a':
+                    AddTodoItem(todoItems);
+                    break;
+                case 'r':
+                    RemoveTodo(todoItems);
+                    break;
+                case 'e':
+                    isRunning = false;
+                    break;
+                default:
+                    Console.WriteLine("Incorrect input.");
+                    break;
+            }
+            Console.WriteLine(); // just some spacing
+        }
+        while (isRunning);
+    }
+
+    static void RemoveTodo(List<string> todoItems)
+    {
+        ListTodoItems(todoItems);
+
+        if (todoItems.Count > 0)
+        {
+
+            Console.WriteLine("Select the index of the TODO you want to remove:");
+            int index;
+            string userInput = Console.ReadLine();
+            bool isCorrectInput = int.TryParse(userInput, out index);
+
+            if (isCorrectInput && index < todoItems.Count)
+            {
+                string choiceItem = todoItems[index - 1];
+                todoItems.RemoveAt(index - 1);
+                Console.WriteLine($"TODO removed: {choiceItem}.");
+            }
+            else
+            {
+                Console.WriteLine("The given index is not valid.");
+                RemoveTodo(todoItems);
+            }
+        }
+    }
+    static void ListTodoItems(List<string> todoItems)
+    {
+        if (todoItems.Count > 0)
+        {
+            for (int i = 0; i < todoItems.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {todoItems[i]}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("No TODOs have been added yet.");
+        }
+    }
+
+    static void AddTodoItem(List<string> todoList)
+    {
+        Console.WriteLine("Enter the TODO description: ");
+        string userInput = Console.ReadLine();
+
+        if (userInput.Length > 0)
+        {
+            if (todoList.Contains(userInput))
+            {
+                Console.WriteLine("The description must be unique.");
+                AddTodoItem(todoList);
+            }
+            else
+            {
+                todoList.Add(userInput);
+                Console.WriteLine($"TODO successfully added: {userInput}.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("The description cannot be empty.");
+            AddTodoItem(todoList);
+        }
+
+    }
+
+    static void DisplayMenu()
+    {
+        Console.WriteLine("Hello");
+        Console.WriteLine("What do you want to do?");
+
+        Console.WriteLine("[S]ee all TODOs");
+        Console.WriteLine("[A]dd a TODO");
+        Console.WriteLine("[R]emove a TODO");
+        Console.WriteLine("[E]xit");
+    }
+}
