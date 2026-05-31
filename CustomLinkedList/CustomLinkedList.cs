09206afde11c6c921c4ffb2ab389d2db1606213c
Miloš Jovanović <milos.jovanovic@lombiq.com>
2026-05-31 02:57:52 +0200

    Custom Linked List

---
+++ CustomLinkedList/CustomLinkedList.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLinkedListImplementation;
+public class CustomLinkedList<T> : ILinkedList<T?>
+{
+    Node<T>? head;
+    private Node<T>? last;
+    private int count = 0;
+
+    public int Count => count;
+
+    public bool IsReadOnly => false;
+
+    public void AddToEnd(T? item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddToFront(T? item)
+    {
+        head = new Node<T>(item) { Next = head };
+        count++;
+    }
+
+    public void Add(T? item)
+    {
+        AddToFront(item);
+    }
+
+    public void Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(T? item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CopyTo(T?[] array, int arrayIndex)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerator<T?> GetEnumerator()
+    {
+        foreach(var node in GetNodes())
+        {
+            yield return node.Value;
+        }
+    }
+
+    public bool Remove(T? item)
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    private IEnumerable<Node<T>> GetNodes()
+    {
+        if(head is null) { yield break; }
+        var currentHead = head;
+        while(currentHead is not null)
+        {
+            yield return currentHead;
+            currentHead = currentHead.Next;
+        }
+    }
+}
---
+++ CustomLinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CustomLinkedListImplementation;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var customLinkedList = new CustomLinkedList<string>();
+        customLinkedList.Add("Milos");
+        customLinkedList.Add("Test");
+        customLinkedList.Add("Test 2");
+        customLinkedList.Add("Test 3");
+        customLinkedList.Add("Test 4");
+        customLinkedList.Add("Test 5");
+
+        foreach(var item in customLinkedList)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine("Hello, World!");
+        Console.ReadKey();
+    }
+}
